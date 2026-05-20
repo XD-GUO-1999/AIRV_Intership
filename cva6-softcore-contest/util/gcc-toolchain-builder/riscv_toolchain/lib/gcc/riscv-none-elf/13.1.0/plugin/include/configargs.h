eaa6bce5fcfde4a912e6261ccaf5d3a3290c7307
imtuser <guoxinda1999@foxmail.com>
2026-05-20 15:59:51 +0200

    add approach1

---
+++ cva6-softcore-contest/util/gcc-toolchain-builder/riscv_toolchain/lib/gcc/riscv-none-elf/13.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../src/gcc/configure --prefix=/home/imtuser/AIRV/cva6-softcore-contest/util/gcc-toolchain-builder/riscv_toolchain --target=riscv-none-elf --enable-languages=c --disable-libssp --disable-libgomp --disable-libmudflap";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32d" }, { "arch", "rv32imafdc_zicsr_zifencei" }, { "isa_spec", "20191213" } };
